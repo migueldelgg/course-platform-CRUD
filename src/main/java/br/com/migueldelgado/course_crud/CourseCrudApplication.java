62389780848e8bff717db4fc7c8dff33d9ba5f92
4Miguel2 <delgdevoleper@gmail.com>
2024-05-28 11:53:02 -0300

    Conexao com banco de dados realizada

---
+++ src/main/java/br/com/migueldelgado/course_crud/CourseCrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.migueldelgado.course_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/migueldelgado/course_crud/CourseCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.migueldelgado.course_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
